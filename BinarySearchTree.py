a40dc3e70d66355ebb4d2c631b01c2611d7a2df5
CloudCrewAtWork <anurag.sai540@gmail.com>
2023-06-13 10:47:26 -0600

    Added different types of Traversions in the code

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -1,16 +1,33 @@
+#Defining a calss Node 
 class Node:
     def __init__(self, val):
-        self.val = val
+        self.value = val
         self.left = None
         self.right = None
 
+#Function to do an InOrderTraversal
 def inOrderTraversal(root):
-    if root != None:
-        inOrderTraversal(root.left)
-        print(root.value, end = " ")
-        inOrderTraversal(root.right)
+    if root is not None:
+        inOrderTraversal(root.right) #Traversing the left Tree
+        print(root.value, end = " ") #Printing the value at the nod3
+        inOrderTraversal(root.left) #Traversing the right Tree
+
+#Function to do an PreOrderTraversal
+def preOrderTraversal(root):
+    if root is not None:
+        print(root.value, end = " ") #Printing the value at the nod3
+        preOrderTraversal(root.left) #Traversing the left Tree
+        preOrderTraversal(root.right) #Traversing the right Tree
+
+#Function to do an PostOrderTraversal
+def postOrdertraversal(root):
+    if root is not None:
+        postOrdertraversal(root.left) #Traversing the left Tree
+        postOrdertraversal(root.right) #Traversing the right Tree
+        print(root.value, end = " ")  #Printing the value at the nod3
 
 
+#Building the Tree:
 root = Node(4)
 root.left = Node(2)
 root.right = Node(6)
@@ -19,3 +36,12 @@ root.left.right = Node(3)
 root.right.left = Node(5)
 root.right.right = Node(7)
 
+# Performing the traversals
+print("In-order Traversal:")
+inOrderTraversal(root)
+
+print("\nPre-order Traversal:")
+preOrderTraversal(root)
+
+print("\nPost-order Traversal:")
+postOrdertraversal(root)
